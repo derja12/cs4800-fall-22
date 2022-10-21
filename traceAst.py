22bf72132cd8d62e5f3674afa321c233e27d391e
Kaleb Petersen <kcpetersen111@gmail.com>
2022-10-21 12:15:11 -0600

    doin stuff

---
+++ traceAst.py
@@ -0,0 +1,38 @@
+#does not work with python 2 so make sure on that one
+import ast
+import astpretty
+# import traceAst
+import trace
+import sys
+
+def test():
+    var = 10 + 10
+    stringVar = "Hello,"
+    stringVar2 = " World!"
+
+    return stringVar + stringVar2 + str(var)
+
+# file = open("all_sorts.py")
+# tree = ast.parse("all_sorts.py", mode='eval')
+# print(ast.dump(tree))
+# print(ast.parse(open("all_sorts.py")))
+
+
+fin = open("all_sorts.py", "r")
+fileContent = ""
+for line in fin:
+    fileContent += line
+node = ast.parse(fileContent)
+fin.close()
+
+astpretty.pprint(node)
+# print(ast.dump(node))
+tracer = trace.Trace(
+    ignoredirs=[sys.prefix, sys.exec_prefix]
+)
+tracer.run(node)
+
+r = tracer.results()
+r.write_results(show_missing=True, coverdir=".")
+# traceAst
+
